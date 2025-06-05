d09fd8c3f472e789906142e687225d1e1db14d0e
Felipe Mendieta <48095820+felipe-mendieta@users.noreply.github.com>
2025-06-05 13:39:58 -0500

    Remove test page and add analysis page with enhanced data visualization and interactivity features

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,17 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import home_page, \
     about_page, \
     explore_page, \
-    document_detail_page, \
-    test_page
-
+    document_detail_page
 app_name = 'core'
 urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
     path('explore/', explore_page, name='explore'),
     path('document_detail/', document_detail_page, name='document_detail'),
-    path('test/', test_page, name='test'),
-    
 ]
